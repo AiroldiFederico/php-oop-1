f7894c4163eba12600b053a138d0f9e416ba26ce
Federico Airoldi <airoldibusiness@gmail.com>
2023-06-03 18:00:11 +0200

    FASE 2 - creo due ogetti film e stampo i valori

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
-/*
- - è definita una classe Movie
-   => all'interno della classe sono dichiarate delle variabili d'istanza
-   => all'interno della classe è definito un costruttore
-   => all'interno della classe è definito almeno un metodo
-- vengono istanziati almeno due oggetti Movie e stampati a schermo i valori delle relative proprietà
 
-*/
 
 <?php
+
+// - è definita una classe Movie
+//    => all'interno della classe sono dichiarate delle variabili d'istanza
+//    => all'interno della classe è definito un costruttore
+//    => all'interno della classe è definito almeno un metodo
+// - vengono istanziati almeno due oggetti Movie e stampati a schermo i valori delle relative proprietà
+
 
 class Movie {
 
@@ -25,20 +25,36 @@ class Movie {
         $this->anno = $anno;
         $this->regista = $regista;
         $this->genere = $genere;
+        $this->setPrezzo($durata);
     }
 
     #metodo
     public function setPrezzo($durata){
         if( $durata > 120 ){
-            $this->prezzoBiglietto = '12$';
+            $this->prezzoBiglietto = '12$'; 
         } else {
             $this->prezzoBiglietto = '9$';
         }
     }
 
-    public function getDurata() {
-        return $this->durata;
+    public function getPrezzo() {
+        return $this->prezzoBiglietto;
     }
 
 }
 
+#creazione ogetti
+$alien = new Movie(117, 1979, "Ridley Scott", "fantascienza, thriller, horror");
+echo "Durata: " . $alien->durata . " minuti" . "<br>";
+echo "Anno: " . $alien->anno . "<br>";
+echo "Regista: " . $alien->regista . "<br>";
+echo "Genere: " . $alien->genere . "<br>";
+echo "Prezzo: " . $alien->getPrezzo() . "<br>";
+
+
+$terminator = new Movie(137, 1984, "James Cameron", "azione, fantascienza, thriller");
+echo "Durata: " . $terminator->durata . " minuti" . "<br>";
+echo "Anno: " . $terminator->anno . "<br>";
+echo "Regista: " . $terminator->regista . "<br>";
+echo "Genere: " . $terminator->genere . "<br>";
+echo "Prezzo: " . $terminator->getPrezzo() . "<br>";
