83ac829f3704f6608667a174d72803b9b82c50d1
Federico Airoldi <airoldibusiness@gmail.com>
2023-06-03 16:11:13 +0200

    FASE 1  - creazione struttura e dichiaro le variabili

---
+++ index.php
@@ -0,0 +1,24 @@
+/*
+ - è definita una classe Movie
+   => all'interno della classe sono dichiarate delle variabili d'istanza
+   => all'interno della classe è definito un costruttore
+   => all'interno della classe è definito almeno un metodo
+- vengono istanziati almeno due oggetti Movie e stampati a schermo i valori delle relative proprietà
+
+*/
+
+<?php
+
+class Movie {
+
+    #variabili
+    public $durata;
+    public $anno;
+    public $regista;
+    public $genere;
+
+
+
+
+}
+
